cfb00924c08d16c44b5f0b5de3c840151ce5ba98
FelippeTN <felippenalim2004@gmail.com>
2025-07-24 19:21:20 -0300

    ADD: Creating an API view and update URL routing

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from core.api.api_view import Llm_ResponseView
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Luna-Chat-API",
@@ -11,7 +13,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(permissions.IsAuthenticated,),
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
@@ -19,4 +21,12 @@ urlpatterns = [
     path('login/', login_view, name='login_view'),
     path('register/', register_view, name='register_view'),
     path('chat/', chat_view, name='chat_view'),
+
+    path('api/llm-response/', Llm_ResponseView.as_view(), name='llm_response'),
+
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
+    path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
