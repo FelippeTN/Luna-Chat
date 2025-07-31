1039d5cb68f7b9b1ccab7817dc1cf61dae691196
FelippeTN <felippenalim2004@gmail.com>
2025-07-31 19:10:15 -0300

    REF: refactor LlmResponseView to use AsyncGroq for streaming responses

--- core/api/api_view.py
+++ core/api/api_view.py
@@ -3,59 +3,22 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-import httpx, json, os
 from dotenv import load_dotenv
+import asyncio, os
+from groq import AsyncGroq
 
 load_dotenv()
-URL_LLAMA = os.getenv("URL_LLAMA")
-LLAMA_V1_COMPLETIONS = f"{URL_LLAMA}/v1/completions"
+GROQ_KEY = os.getenv("GROQ_KEY")
 
 
 class LlmResponseView(APIView):
     permission_classes = [IsAuthenticated]
-
-    async def _stream_llama_response(self, payload):
-        headers = {'Content-Type': 'application/json'}
-        try:
-            async with httpx.AsyncClient() as client:
-                async with client.stream("POST", LLAMA_V1_COMPLETIONS, json=payload, headers=headers) as response:
-                    response.raise_for_status()
-
-                    async for line in response.aiter_lines():
-                        if line.startswith("data: "):
-                            json_str = line[6:]
-
-                            if json_str == "[DONE]":
-                                break
-
-                            try:
-                                data = json.loads(json_str)
-                                delta = data.get('choices', [{}])[0].get('delta', {})
-                                content_chunk = delta.get('content')
-
-                                if content_chunk:
-                                    yield content_chunk
-                            except (json.JSONDecodeError, IndexError, KeyError):
-                                continue
-
-        except httpx.RequestError as e:
-            yield f"Error connecting to AI service: {e}"
-        except httpx.HTTPStatusError as e:
-            yield f"AI service returned an error: {e.response.status_code}"
-
-    async def post(self, request):
-        user_message = request.data.get("message")
-
-        use_stream = request.query_params.get("stream", "false").lower() == "true"
+    
+    async def _stream_groq_response(self, user_message):
+        client = AsyncGroq(api_key=GROQ_KEY)
         
-        if not user_message:
-            return Response(
-                {"error": "O campo 'message' não pode ser vazio."},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-            
-        payload = {
-            "messages": [
+        chat_completion = await client.chat.completions.create(
+            messages =[
                 {
                     "role": "system",
                     "content": "Você é um assistente de IA chamada Luna, desenvolvida por Felippe Toscano Nalim. Você deve responder de forma clara e objetiva, sempre buscando a melhor resposta possível para o usuário."
@@ -65,29 +28,32 @@ class LlmResponseView(APIView):
                     "content": user_message
                 }
             ],
-            "temperature": 0.7,
-            "stream": use_stream
-        }
-        if use_stream:
+            model="llama-3.3-70b-versatile",
+            temperature=0.5,
+            top_p=1,
+            stop=None,
+            stream=True
+            )
+            
+        for chunk in chat_completion:
+            print(chunk.choices[0].delta.content, end='')
+            yield chunk.choices[0].delta.content
+        
+
+    async def post(self, request):
+        user_message = request.data.get("message")
+        
+        if not user_message:
+            return Response(
+                {"error": "O campo 'message' não pode ser vazio."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
+        if user_message:
             return StreamingHttpResponse(
-                self._stream_llama_response(payload),
+                self._stream_groq_response(user_message),
                 status=status.HTTP_200_OK,
                 content_type='text/plain'
             )
-        else:
-            headers = {'Content-Type': 'application/json'}
-            try:
-                async with httpx.AsyncClient(timeout=60.0) as client:
-                    response = await client.post(LLAMA_V1_COMPLETIONS, json=payload, headers=headers)
-                    response.raise_for_status()
-                
-                response_data = response.json()
-                ai_message_content = response_data.get('choices', [{}])[0].get('message', {}).get('content', '')
-                return Response({"message": ai_message_content}, status=status.HTTP_200_OK)
-
-            except Exception as e:
-                return Response(
-                    {"error": f"Erro ao se comunicar com o serviço de IA: {str(e)}"},
-                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
-                )
+        
 
