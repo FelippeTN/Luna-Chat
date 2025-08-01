493999086b74c5bef9eaf82fc5226e719769b648
FelippeTN <felippenalim2004@gmail.com>
2025-08-01 14:19:57 -0300

    ADD: UserConversation model to manage user conversations and messages

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class UserConversation(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    conversation_id = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    messages = models.JSONField(default=list)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.conversation_id}"
+    
+    def add_message(self, message):
+        self.messages.append(message)
+        self.save()
